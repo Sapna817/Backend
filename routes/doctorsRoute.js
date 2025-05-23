68354ba9e41c2104402bbe9d5643696656f62d43
Sapna817 <110749363+Sapna817@users.noreply.github.com>
2025-05-23 21:51:41 +0530

    Update doctorsRoute.js

--- routes/doctorsRoute.js
+++ routes/doctorsRoute.js
@@ -2,6 +2,7 @@ import express from "express";
 import Doctor from "../models/doctors.js";
 import { getDoctorByName } from "../controllers/doctorControllers.js";
 import { loginDoctor } from "../controllers/doctorControllers.js";
+import { getMe } from "../controllers/doctorControllers.js";
 
 const router = express.Router();
 
@@ -19,6 +20,7 @@ router.get('/doctors', async (req, res) => {
 
 router.get("/doctors/name/:name", getDoctorByName);
 router.post('/doctor/login', loginDoctor);
+router.get("/doctor/me", getMe);
 
 
 export default router;
